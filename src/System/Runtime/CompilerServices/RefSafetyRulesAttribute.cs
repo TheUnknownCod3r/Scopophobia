abb0d0583d57455ab7b9353a471b1c76cca03d19
TheUnknownCod3r <114740383+TheUnknownCod3r@users.noreply.github.com>
2024-04-21 23:30:54 +0100

    Clean Files

--- src/System/Runtime/CompilerServices/RefSafetyRulesAttribute.cs
+++ src/System/Runtime/CompilerServices/RefSafetyRulesAttribute.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: System.Runtime.CompilerServices.RefSafetyRulesAttribute
-// Assembly: Scopophobia, Version=1.1.1.0, Culture=neutral, PublicKeyToken=null
-// MVID: 832B2E03-5C0C-4019-8402-65D9E2D1AD81
-// Assembly location: G:\jaspercreations-Scopophobia-1.1.1\Scopophobia\Scopophobia.dll
-
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 namespace System.Runtime.CompilerServices
