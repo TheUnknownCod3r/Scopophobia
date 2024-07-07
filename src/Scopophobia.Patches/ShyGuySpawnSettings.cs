4a6659e61e46351dfff63d98d285a45c8e988272
TheUnknownCod3r <114740383+TheUnknownCod3r@users.noreply.github.com>
2024-07-07 04:21:41 +0100

    Revert spawn patch
    What a complete fudge up

--- src/Scopophobia.Patches/ShyGuySpawnSettings.cs
+++ src/Scopophobia.Patches/ShyGuySpawnSettings.cs
@@ -25,25 +25,25 @@ internal class ShyGuySpawnSettings
 			for (int i = 0; i < ___currentLevel.Enemies.Count; i++)
 			{
 				SpawnableEnemyWithRarity val2 = ___currentLevel.Enemies[i];
-				if (val2.enemyType.enemyName == "Shy guy")
-				{
-					enemies.Remove(val2);
+					if (val2.enemyType.enemyName == "Shy guy")
+					{
+						enemies.Remove(val2);
+						
+					}					
 				}
-			}
-			___currentLevel.Enemies = enemies;
-			shyEnemy.enemyType.PowerLevel = Config.ShyGuyPowerLevel; //change from int to float
-			shyEnemy.rarity = Config.spawnRarity;
-			shyEnemy.enemyType.probabilityCurve = new AnimationCurve(new Keyframe(0f, Config.startEnemySpawnCurve), new Keyframe(0.5f, Config.midEnemySpawnCurve), new Keyframe(1f, Config.endEnemySpawnCurve));
-			shyEnemy.enemyType.MaxCount = Config.maxSpawnCount;
-			shyEnemy.enemyType.isOutsideEnemy = Config.canSpawnOutside;
-			if (Config.canSpawnOutside & (!Config.spawnOutsideHardPlanets || !nonoOutside.Contains(___currentLevel.sceneName)))
-			{
-				___currentLevel.OutsideEnemies.Add(shyEnemy);
-				SelectableLevel obj = ___currentLevel;
-				obj.maxOutsideEnemyPowerCount += shyEnemy.enemyType.MaxCount * (int)shyEnemy.enemyType.PowerLevel; //typecast as int to fix PowerLevel, ty MaskedOverhaulFork
-					ScopophobiaPlugin.logger.LogInfo("Theres a Shy Guy outside... Proceed with caution"); //Test to see if Shy guy spawns outside, see below
-			}
-			if (!___currentLevel.OutsideEnemies.Contains(shyEnemy)) { ___currentLevel.Enemies.Add(shyEnemy); ScopophobiaPlugin.logger.LogInfo("Enemy not found Outside, Spawning Inside"); }//Check for outside enemy existing in level
+            ___currentLevel.Enemies = enemies;
+            shyEnemy.enemyType.PowerLevel = Config.ShyGuyPowerLevel; //change from int to float
+            shyEnemy.rarity = Config.spawnRarity;
+            shyEnemy.enemyType.probabilityCurve = new AnimationCurve(new Keyframe(0f, Config.startEnemySpawnCurve), new Keyframe(0.5f, Config.midEnemySpawnCurve), new Keyframe(1f, Config.endEnemySpawnCurve));
+            shyEnemy.enemyType.MaxCount = Config.maxSpawnCount;
+            shyEnemy.enemyType.isOutsideEnemy = Config.canSpawnOutside;
+				if (Config.canSpawnOutside & (!Config.spawnOutsideHardPlanets || !nonoOutside.Contains(___currentLevel.sceneName)))
+				{
+					___currentLevel.OutsideEnemies.Add(shyEnemy);
+					SelectableLevel obj = ___currentLevel;
+					obj.maxOutsideEnemyPowerCount += shyEnemy.enemyType.MaxCount * (int)shyEnemy.enemyType.PowerLevel; //typecast as int to fix PowerLevel, ty MaskedOverhaulFork
+				}
+				___currentLevel.Enemies.Add(shyEnemy);
 		} catch { }
 	}
 } }
